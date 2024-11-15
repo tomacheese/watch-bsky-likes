60f9fdac107c381fa61e7fed3accca9b7744965c
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2024-11-15 13:42:41 +0900

    feat: 通知対象のポストを画像付きのみとする

--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,7 @@
-import axios from 'axios'
 import { Notified } from './notified'
 import { Configuration } from './config'
 import { Discord, Logger } from '@book000/node-utils'
-
-interface Like {
-  uri: string
-  cid: string
-  value: {
-    $type: 'app.bsky.feed.like'
-    subject: {
-      cid: string
-      uri: string
-    }
-    createdAt: string
-  }
-}
-
-interface LikeResponse {
-  records: Like[]
-  cursor: string
-}
-
-async function getUserLikes(actor: string) {
-  const baseUrl = 'https://bsky.social/xrpc/com.atproto.repo.listRecords'
-  const response = await axios.get<LikeResponse>(baseUrl, {
-    params: {
-      collection: 'app.bsky.feed.like',
-      limit: 100,
-      reverse: false,
-      cursor: '',
-      repo: actor,
-    },
-  })
-
-  return response.data
-}
-
-async function getPostDetail(uris: string) {
-  const baseUrl = 'https://public.api.bsky.app/xrpc/app.bsky.feed.getPosts'
-  const response = await axios.get<LikeResponse>(baseUrl, {
-    params: {
-      uris,
-    },
-  })
-
-  return response.data
-}
-
-function getPostUrl(uri: string) {
-  const regex = /at:\/\/(.+)\/app\.bsky\.feed\.post\/(.+)/
-
-  const match = regex.exec(uri)
-  if (!match) {
-    console.log('No match', uri)
-    return
-  }
-
-  const actor = match[1]
-  const cid = match[2]
-
-  return `https://bsky.app/profile/${actor}/post/${cid}`
-}
+import { Bluesky } from './bsky'
 
 async function main() {
   const logger = Logger.configure('main')
@@ -79,24 +20,38 @@ async function main() {
 
   const isFirst = Notified.isFirst()
 
-  const results = await getUserLikes('hiratake.dev')
+  const results = await Bluesky.getUserLikes('hiratake.dev')
   const filteredResults = results.records.filter((record) => {
     const { uri } = record.value.subject
     return !Notified.isNotified(uri)
   })
 
+  const postDetails = await Bluesky.getPosts(
+    filteredResults.map((r) => r.value.subject.uri)
+  )
+
   for (const record of filteredResults) {
     const { uri } = record.value.subject
     const createdAt = record.value.createdAt
 
-    const postUrl = getPostUrl(uri)
+    const postUrl = Bluesky.getPostUrl(uri)
     if (!postUrl) {
       continue
     }
 
+    console.log(`liked your post: ${postUrl} at ${createdAt}`)
+
+    const postDetail = postDetails.find((p) => p.uri === uri)
+    if (!postDetail) {
+      console.log(`  -> Skip because postDetail is undefined`)
+      continue
+    }
     Notified.addNotified(uri)
 
-    console.log(`liked your post: ${postUrl} at ${createdAt}`)
+    if (!Bluesky.isImagePost(postDetail)) {
+      console.log(`  -> Skip because postDetail is not an image post`)
+      continue
+    }
 
     if (isFirst) {
       continue
